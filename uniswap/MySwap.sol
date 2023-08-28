d91759fb65ac4099cd4216d0d30398ef56c2b171
gu <13598957715@126.com>
2023-08-28 20:05:19 +0800

    修改合约

--- uniswap/MySwap.sol
+++ uniswap/MySwap.sol
@@ -179,58 +179,6 @@ contract FengshuAllRouter {
         return (reserveIn,reserveOut);
     }
 
-    //获取交易买入卖出后的涨幅或者跌幅（百分比）(tokenA:bnb || usdt || busd    tokenB:token)
-    function getTradePriceImpact(address[] memory _targetPath, address _targetRouter, uint _targetAmountIn, address tokenA, address tokenB, uint tradeType) public view returns (uint _priceImpact){
-        uint beforePrice = getTokenPrice(_targetRouter, tokenA, tokenB);
-        //使用结构体，解决stack too deep错误
-        TradeOrdered memory tradeOrdered;
-        tradeOrdered._targetPath = _targetPath;
-        tradeOrdered._targetRouter = _targetRouter;
-        tradeOrdered._targetAmountIn = _targetAmountIn;
-        tradeOrdered.tokenA = tokenA;
-        tradeOrdered.tokenB = tokenB;
-        tradeOrdered.tradeType = tradeType;
-
-        (uint reserveIn, uint reserveOut) = getReserves(tradeOrdered._targetRouter, tradeOrdered.tokenA, tradeOrdered.tokenB);
-        uint input = 1000000000000000;//bnb || busd || usdt
-        uint tokenDecimals = uint(IERC20(tradeOrdered.tokenB).decimals());
-        uint amountOut2;
-        uint buyOrSell = 0;//买还是卖 0：买 1：卖
-        uint afterPrice = 0;
-        if(tradeOrdered._targetPath[0] == tradeOrdered.tokenB){
-            //卖单
-            buyOrSell = 1;
-            //tradeType 0:  输入卖出代币数 来获取bnb  1：输入能获得的bnb数  来卖出代币
-            if(tradeOrdered.tradeType == 0){
-                uint amountOut = getAmountOut(tradeOrdered._targetRouter, tradeOrdered._targetAmountIn, reserveOut, reserveIn);
-                amountOut2 = getAmountOut(tradeOrdered._targetRouter, input, reserveIn.sub(amountOut), reserveOut.add(tradeOrdered._targetAmountIn));
-            }else{
-                //这里传参的_amountIn 实际上是amountOut 注:这个amountOut是路径最终获取的代币的数量
-                uint amountOut = tradeOrdered._targetAmountIn;
-                uint[] memory amounts = getAmountsIn(tradeOrdered._targetRouter, amountOut, tradeOrdered._targetPath);//0: 是传入token数量 1：token 换取[1]代币 的数量
-                amountOut2 = getAmountOut(tradeOrdered._targetRouter, input, reserveIn.sub(amounts[1]), reserveOut.add(amounts[0]));
-            }
-        }else if(tradeOrdered._targetPath[tradeOrdered._targetPath.length - 1] == tradeOrdered.tokenB){
-            //买单
-            //tradeType 0:  输入购买(bnb || usdt || busd) 数量 来获取代币  1：输入能获得的代币数  来买入代币
-            if(tradeOrdered.tradeType == 0){
-                uint[] memory amounts = getAmountsOut(tradeOrdered._targetRouter, tradeOrdered._targetAmountIn, tradeOrdered._targetPath);
-                amountOut2 = getAmountOut(tradeOrdered._targetRouter, input, reserveIn.add(amounts[tradeOrdered._targetPath.length - 2]), reserveOut.sub(amounts[tradeOrdered._targetPath.length - 1]));
-            }else{
-                //这里传参的_amountIn 实际上是amountOut 注:这个amountOut是路径最终获取的代币的数量
-                uint amountOut = tradeOrdered._targetAmountIn;
-                uint[] memory amounts = getAmountsIn(tradeOrdered._targetRouter, amountOut, tradeOrdered._targetPath);
-                amountOut2 = getAmountOut(tradeOrdered._targetRouter, input, reserveIn.add(amounts[tradeOrdered._targetPath.length - 2]), reserveOut.sub(amountOut));
-            }
-        }
-        if(tokenDecimals < 18){
-            afterPrice = input.mul(10 ** 18).div(amountOut2.mul(10 ** (18 - tokenDecimals)));
-        }else{
-            afterPrice = input.mul(10 ** 18).div(amountOut2);
-        }
-        return buyOrSell == 0 ? afterPrice.sub(beforePrice).mul(10000).div(beforePrice) : (beforePrice.sub(afterPrice).mul(10000).div(beforePrice));
-    }
-
     //获取当前代币的价格 (tokenA:bnb || usdt || busd  tokenB:token)
     function getTokenPrice(address _router, address tokenA, address tokenB) public view returns (uint _price){
         uint reserveInput = 1000000000000000;
@@ -246,74 +194,6 @@ contract FengshuAllRouter {
              price = reserveInput.mul(10 ** 18).div(amounts[1]);
         }
         return price;
-    }
-
-    //检查代币买卖税
-    function checkHoneypots(address _router, address tokenA, address tokenB, uint amountIn) external {
-        address[] memory _path = new address[](2);
-        _path[0] = tokenA;
-        _path[1] = tokenB;
-        uint[] memory amounts = getAmountsOut(_router, amountIn, _path);
-        uint buyExpectedOut = amounts[1];
-        testBuy(_router, tokenA, tokenB, amountIn);
-        uint buyActualOut = IERC20(tokenB).balanceOf(address(this));
-        //sell
-        uint beforeBnb = IERC20(tokenA).balanceOf(address(this));
-        address[] memory _path2 = new address[](2);
-        _path2[0] = tokenB;
-        _path2[1] = tokenA;
-        uint[] memory amounts2 = getAmountsOut(_router, buyActualOut, _path2);
-        uint sellExpectedOut = amounts2[1];
-        testSellAll(_router, tokenB, tokenA);
-        uint afterBnb = IERC20(tokenA).balanceOf(address(this));
-        uint sellActualOut = afterBnb.sub(beforeBnb);
-        require(1!=1,string(abi.encodePacked("printTax:", uint2str(buyExpectedOut), "-", uint2str(buyActualOut), "-", uint2str(sellExpectedOut), "-", uint2str(sellActualOut))));
-    }
-
-    //printTax:3530138005344,3406583174809,960181031119,926482037394
-    //检查代币买卖税
-    function checkHoneypotsbk(address _router, address tokenA, address tokenB, uint amountIn) external {
-        address[] memory _path = new address[](2);
-        _path[0] = tokenA;
-        _path[1] = tokenB;
-        uint[] memory amounts = getAmountsOut(_router, amountIn, _path);
-        uint buyExpectedOut = amounts[1];
-        testBuy(_router, tokenA, tokenB, amountIn);
-        uint buyActualOut = IERC20(tokenB).balanceOf(address(this));
-        //sell
-        uint beforeBnb = IERC20(tokenA).balanceOf(address(this));
-        address[] memory _path2 = new address[](2);
-        _path2[0] = tokenB;
-        _path2[1] = tokenA;
-        uint[] memory amounts2 = getAmountsOut(_router, buyActualOut, _path2);
-        uint sellExpectedOut = amounts2[1];
-        testSellAll(_router, tokenB, tokenA);
-        uint afterBnb = IERC20(tokenA).balanceOf(address(this));
-        uint sellActualOut = afterBnb.sub(beforeBnb);
-        require(1!=1,string(abi.encodePacked("printTax:", uint2str(buyExpectedOut), ",", uint2str(buyActualOut), ",", uint2str(sellExpectedOut), ",", uint2str(sellActualOut))));
-    }
-
-    function buyTokenSwapCheckHoneypots(address _router, address tokenA, address tokenB, uint amountIn, uint taxFee) external gasTokenRefund{
-        require(whiteList[msg.sender], "not on the white list");
-        address[] memory _path = new address[](2);
-        _path[0] = tokenA;
-        _path[1] = tokenB;
-        uint[] memory amounts = getAmountsOut(_router, amountIn, _path);
-        uint buyExpectedOut = amounts[1];
-        //buy
-        address pairAddress = getPair(_router, tokenA, tokenB);
-        TransferHelper.safeTransfer(tokenA, pairAddress, amountIn);
-        address token0 = tokenA < tokenB ? tokenA : tokenB;
-        (uint amount0Out, uint amount1Out) = tokenA == token0 ? (uint(0), amounts[1]) : (amounts[1], uint(0));
-        IUniswapV2Pair(pairAddress).swap(amount0Out, amount1Out, address(this), new bytes(0));
-        //check buyTax
-        uint buyActualOut = IERC20(tokenB).balanceOf(address(this));
-        require(buyActualOut >= buyExpectedOut.sub(buyExpectedOut.mul(taxFee).div(10**2)), "buy tax too big");
-        //sell
-        uint sellToken = buyActualOut.div(100);//卖出百分之1
-        //自己转给自己验证是否被拉黑以及验证税(有些币转账会扣税)
-        TransferHelper.safeTransfer(tokenB, address(this), sellToken);
-        require(sellToken.mul(taxFee).div(10**2) >= buyActualOut.sub(IERC20(tokenB).balanceOf(address(this))), "sell tax too big");
     }
 
     function buyTokenSwap(address _router, address tokenA, address tokenB, uint amountIn) external gasTokenRefund{
